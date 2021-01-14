46af63e3a9ecf296e08b43c3780e03d5ace08ece
Simurgh101(Scotth) <suthiwatpraesi@hotmail.com>
2021-01-14 18:52:30 +1100

    58A

---
+++ 58A.cpp
@@ -0,0 +1,76 @@
+#include <utility>
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <array>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <cctype>
+#include <string>
+#include <cstring>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+#define SCD(t) scanf("%d", &t)
+#define SCLD(t) scanf("%ld", &t)
+#define SCLLD(t) scanf("%lld", &t)
+#define SCC(t) scanf("%c", &t)
+#define SCS(t) scanf("%s", t)
+#define SCF(t) scanf("%f", &t)
+#define SCLF(t) scanf("%lf", &t)
+#define MEM(a, b) memset(a, (b), sizeof(a))
+#define FOR(i, k) for (int i = 0; i < k; i++)
+#define RFOR(i, k) (int i = 0; i >= k; i--)
+#define all(cont) cont.begin(), cont.end()
+#define rall(cont) cont.end(), cont.begin()
+#define EACH(it, l) for (auto it = l.begin(); it != l.end(); it++)
+#define IN(A, B, C) assert(B <= A && A <= C)
+#define MP make_pair
+#define PB push_back
+#define INF (int)1e9
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+#define read(type) readInt<type>()
+#define print(t) cout << t;
+
+int main()
+{
+
+    int count = 0;
+    string h = "hello", input;
+    cin >> input;
+    for (int i = 0; i < input.size(); i++)
+    {
+        if (input[i] == h[count])
+        {
+            count++;
+            // cout << "pass" << endl;
+        }
+    }
+    if (count == 5)
+    {
+        cout << "YES";
+    }
+    else
+    {
+        cout << "NO";
+    }
+}
