252b50cfb1755d079cc4eb4276d025120724d17c
Simurgh101(Scotth) <suthiwatpraesi@hotmail.com>
2021-01-14 18:25:47 +1100

    fix tem

--- Templates/A.cpp
+++ Templates/A.cpp
@@ -28,20 +28,20 @@
 
 using namespace std;
 
-#define SCD(t) scanf("%d",&t)
-#define SCLD(t) scanf("%ld",&t)
-#define SCLLD(t) scanf("%lld",&t)
-#define SCC(t) scanf("%c",&t)
-#define SCS(t) scanf("%s",t)
-#define SCF(t) scanf("%f",&t)
-#define SCLF(t) scanf("%lf",&t)
+#define SCD(t) scanf("%d", &t)
+#define SCLD(t) scanf("%ld", &t)
+#define SCLLD(t) scanf("%lld", &t)
+#define SCC(t) scanf("%c", &t)
+#define SCS(t) scanf("%s", t)
+#define SCF(t) scanf("%f", &t)
+#define SCLF(t) scanf("%lf", &t)
 #define MEM(a, b) memset(a, (b), sizeof(a))
-#define FOR(i,k) for (int i=0 ; i<k ; i++)
-#define RFOR(i,k) (int i=0; i>=k ; i--)
+#define FOR(i, k) for (int i = 0; i < k; i++)
+#define RFOR(i, k) (int i = 0; i >= k; i--)
 #define all(cont) cont.begin(), cont.end()
 #define rall(cont) cont.end(), cont.begin()
 #define EACH(it, l) for (auto it = l.begin(); it != l.end(); it++)
-#define IN(A, B, C) assert( B <= A && A <= C)
+#define IN(A, B, C) assert(B <= A && A <= C)
 #define MP make_pair
 #define PB push_back
 #define INF (int)1e9
@@ -51,10 +51,9 @@ using namespace std;
 #define read(type) readInt<type>()
 #define print(t) cout << t;
 
-
 int main()
 {
-  int arr[4]= {1,2,3,3} 
-  EACH(it,arr) print(i);
+  int arr[4] = {1, 2, 3, 3};
+  FOR(it, 4) { print(it); }
   return 0;
 }
